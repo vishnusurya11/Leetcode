35e99c09d8051e04f1d9274037f4616ad2a1f499
Vishnu Surya Reddy Nandi <vishnusurya11@gmail.com>
2017-12-28 18:20:06 -0800

    Update 1_Two Sum.py

--- 1_Two Sum.py
+++ 1_Two Sum.py
@@ -8,5 +8,8 @@ class Solution:
        
         for i in range(len(nums)):
             c= target - nums[i]
+            if (nums.count(c) >1 ):
+                return[i,nums[i+1:].index(c)+1+i]
             if( c in nums[i+1:]):
                 return [i,nums.index(c)]
+       
